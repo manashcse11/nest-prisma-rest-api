67d4164340b50f50acf959b3f496b0839b6f1aca
Manash <manash@cefalo.com>
2024-11-26 16:33:06 +0600

    Integrate Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /** Swagger */
+  const config = new DocumentBuilder()
+    .setTitle('Nest - Prisma - Mysql - REST')
+    .setDescription('Learning NestJS with Prisma integrations')
+    .setVersion('0.1')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+  /** End */
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
